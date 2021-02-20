d7ebb36926c858779fb9e98bc0a4e83618ec47cf
ItsRish06 <66073563+ItsRish06@users.noreply.github.com>
2021-02-21 01:59:09 +0530

    added pagination

--- server-side/scholarships/api/views.py
+++ server-side/scholarships/api/views.py
@@ -7,6 +7,7 @@ from rest_framework import generics
 import datetime
 from ..forms import ScholarshipFilter
 from django_filters import rest_framework as filters
+from django.core.paginator import Paginator
 
 
 class api_filter_scholarship(generics.ListAPIView):
@@ -32,11 +33,15 @@ def api_state_list_scholarship(request,state):
     try:
         sch = Scholarship.objects.all()
         qs = sch.filter(state__name__icontains=state).filter(deadline__gte = datetime.datetime.now().strftime("%Y-%m-%d") ).order_by('deadline')
-    except Scholarship.DoesNotExist :
+        paginator = Paginator(qs, 18)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+    except Scholarship.DoesNotExist:
         return Response(status.HTTP_404_NOT_FOUND)
 
-    serializer = ScholarshipListSerializer(qs,many = True)
-    return Response(serializer.data) 
+    serializer = ScholarshipListSerializer(page_obj,many = True)
+    return Response(serializer.data)
  
 
 @api_view(['GET',])
@@ -44,10 +49,14 @@ def api_class_list_scholarship(request,sclass):
     try:
         sch = Scholarship.objects.all()
         qs = sch.filter(sclass__name__icontains=sclass).filter(deadline__gte = datetime.datetime.now().strftime("%Y-%m-%d") ).order_by('deadline')
-    except Scholarship.DoesNotExist :
+        paginator = Paginator(qs, 18)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+    except Scholarship.DoesNotExist:
         return Response(status.HTTP_404_NOT_FOUND)
 
-    serializer = ScholarshipListSerializer(qs,many = True)
+    serializer = ScholarshipListSerializer(page_obj,many = True)
     return Response(serializer.data) 
  
 
@@ -56,10 +65,14 @@ def api_type_list_scholarship(request,stype):
     try:
         sch = Scholarship.objects.all()
         qs = sch.filter(stype__icontains=stype).filter(deadline__gte = datetime.datetime.now().strftime("%Y-%m-%d") ).order_by('deadline')
-    except Scholarship.DoesNotExist :
+        paginator = Paginator(qs, 18)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+    except Scholarship.DoesNotExist:
         return Response(status.HTTP_404_NOT_FOUND)
 
-    serializer = ScholarshipListSerializer(qs,many = True)
+    serializer = ScholarshipListSerializer(page_obj,many = True)
     return Response(serializer.data)
 
 
@@ -67,21 +80,29 @@ def api_type_list_scholarship(request,stype):
 def api_list_active_scholarship(request):
     try:
         qs = Scholarship.objects.all().filter(deadline__gte = datetime.datetime.now().strftime("%Y-%m-%d") ).order_by('deadline')
+        paginator = Paginator(qs, 18)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
     except Scholarship.DoesNotExist:
         return Response(status.HTTP_404_NOT_FOUND)
 
-    serializer = ScholarshipListSerializer(qs,many = True)
-    return Response(serializer.data) 
+    serializer = ScholarshipListSerializer(page_obj,many = True)
+    return Response(serializer.data)
 
 
 @api_view(['GET',])
 def api_list_inactive_scholarship(request):
     try:
         qs = Scholarship.objects.all().exclude(deadline__gte = datetime.datetime.now().strftime("%Y-%m-%d") ).order_by('-deadline')
+        paginator = Paginator(qs, 18)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
     except Scholarship.DoesNotExist:
         return Response(status.HTTP_404_NOT_FOUND)
 
-    serializer = ScholarshipListSerializer(qs,many = True)
+    serializer = ScholarshipListSerializer(page_obj,many = True)
     return Response(serializer.data) 
 
 
@@ -90,8 +111,12 @@ def api_category_list_scholarship(request,category):
     try:
         sch = Scholarship.objects.all()
         qs = sch.filter(category__icontains=category).filter(deadline__gte = datetime.datetime.now().strftime("%Y-%m-%d") ).order_by('deadline')
-    except Scholarship.DoesNotExist :
+        paginator = Paginator(qs, 18)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+    except Scholarship.DoesNotExist:
         return Response(status.HTTP_404_NOT_FOUND)
 
-    serializer = ScholarshipListSerializer(qs,many = True)
+    serializer = ScholarshipListSerializer(page_obj,many = True)
     return Response(serializer.data) 
